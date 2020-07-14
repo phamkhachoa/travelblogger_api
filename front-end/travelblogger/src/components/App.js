91e846c0e6eac0b51d2a75c32a9eaa525e138bb0
phamkhachoa <phamkhachoabk@gmail.com>
2020-07-15 01:00:59 +0700

    cmt

--- front-end/travelblogger/src/components/App.js
+++ front-end/travelblogger/src/components/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import '../App.css';
+import Header from './Header'
+import TopBar from './TopBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopBar />
+      <Header />
     </div>
   );
 }
